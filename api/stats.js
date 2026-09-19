684aa487a8c5d3457859b53388e613d04fe3f614
prime-purge-tech <primepurgetech@gmail.com>
2026-09-19 21:32:47 +0100

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,9 +1,6 @@
 import { redis } from '../lib/db.js';
-
-// Comptes créés sur le site + nombre de fichiers APK / ZIP publiés
 export default async (req, res) => {
-  const [accounts, items] = await Promise.all([redis.hlen('accounts'), redis.hgetall('items')]);
-  const files = Object.values(items || {}).filter(i => /\.(apk|zip)$/i.test(i.name || '')).length;
-  res.setHeader('Cache-Control', 's-maxage=15');
+  const [accounts, files] = await Promise.all([redis.hlen('users'), redis.hlen('items')]);
+  res.setHeader('Cache-Control', 's-maxage=30');
   res.json({ accounts, files });
 };
