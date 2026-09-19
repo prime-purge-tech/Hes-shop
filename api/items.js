2dd76495b5b9936fe894e10b70fa213a67366336
prime-purge-tech <primepurgetech@gmail.com>
2026-09-19 21:33:33 +0100

    Update items.js

--- api/items.js
+++ api/items.js
@@ -3,7 +3,7 @@ export default async (req, res) => {
   const lk = await redis.hgetall('likes') || {};
   const items = Object.values(await redis.hgetall('items') || {})
     .sort((a, b) => b.ts - a.ts)
-    .map(({ file, photo, ...p }) => ({ ...p, likes: Number(lk[p.id]) || 0 }));   // file_id jamais exposés
+    .map(({ file, photo, ...p }) => ({ ...p, hasImg: !!photo, likes: Number(lk[p.id]) || 0 }));   // file_id jamais exposés
   res.setHeader('Cache-Control', 's-maxage=15');
   res.json({ bot: process.env.BOT_USERNAME, items });
 };
