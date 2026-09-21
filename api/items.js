a4abe5b334e8757f87dae3b18c4dae4bcf844de5
prime-purge-tech <primepurgetech@gmail.com>
2026-09-21 01:51:06 +0100

    Update items.js

--- api/items.js
+++ api/items.js
@@ -1,10 +1,11 @@
 import { redis, brandName } from '../lib/db.js';
 export default async (req, res) => {
-  const [lk, dl] = await Promise.all([redis.hgetall('likes'), redis.hgetall('dls')]);
+  const [lk, dl, rat] = await Promise.all([redis.hgetall('likes'), redis.hgetall('dls'), redis.hgetall('rat')]);
   const items = Object.values(await redis.hgetall('items') || {})
     .sort((a, b) => b.ts - a.ts)
     .map(({ file, photo, shots, ...p }) => ({   // file_id jamais exposés
       ...p, hasImg: !!photo, ns: (shots || []).length, dn: brandName(p.title, p.name),
+      rt: (rat || {})[p.id] ? +((rat[p.id].s) / rat[p.id].n).toFixed(1) : 0, rn: (rat || {})[p.id]?.n || 0,
       likes: Number((lk || {})[p.id]) || 0, dls: Number((dl || {})[p.id]) || 0,
     }));
   res.setHeader('Cache-Control', 's-maxage=15');
