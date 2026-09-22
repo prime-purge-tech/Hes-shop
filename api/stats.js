9a4d9702b8a3b6b69f5f99cedc4324b02da5f1a7
prime-purge-tech <primepurgetech@gmail.com>
2026-09-22 02:01:45 +0100

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,6 +1,9 @@
 import { redis } from '../lib/db.js';
+const sum = o => Object.values(o || {}).reduce((a, v) => a + (Number(v) || 0), 0);
 export default async (req, res) => {
-  const [accounts, files] = await Promise.all([redis.hlen('users'), redis.hlen('items')]);
+  const [accounts, files, dls, likes] = await Promise.all([
+    redis.hlen('users'), redis.hlen('items'), redis.hgetall('dls'), redis.hgetall('likes'),
+  ]);
   res.setHeader('Cache-Control', 's-maxage=30');
-  res.json({ accounts, files });
+  res.json({ accounts, files, downloads: sum(dls), likes: sum(likes) });
 };
