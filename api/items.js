143f73c8e043b5bb862da0c3d8a6c500e1aa4d11
prime-purge-tech <primepurgetech@gmail.com>
2026-09-24 00:20:48 +0100

    Update items.js

--- api/items.js
+++ api/items.js
@@ -3,7 +3,7 @@ export default async (req, res) => {
   const [lk, dl, rat, res_] = await Promise.all([redis.hgetall('likes'), redis.hgetall('dls'), redis.hgetall('rat'), redis.hgetall('restrict')]);
   const items = Object.values(await redis.hgetall('items') || {})
     .sort((a, b) => b.ts - a.ts)
-    .map(({ file, photo, shots, ...p }) => ({   // file_id jamais exposés
+    .map(({ file, photo, shots, blob, ...p }) => ({   // file_id et lien du stockage jamais exposés
       ...p, hasImg: !!photo, ns: (shots || []).length, dn: brandName(p.title, p.name), badge: (res_ || {})[p.id] || '',
       rt: (rat || {})[p.id] ? +((rat[p.id].s) / rat[p.id].n).toFixed(1) : 0, rn: (rat || {})[p.id]?.n || 0,
       likes: Number((lk || {})[p.id]) || 0, dls: Number((dl || {})[p.id]) || 0,
